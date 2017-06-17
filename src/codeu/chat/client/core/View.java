06761bccde8ace5fff3feaee9755d0b2bad1d2eb
willyjlee <williamjlee@ucla.edu>
2017-06-16 18:03:36 -0700

    remove extra comment

--- src/codeu/chat/client/core/View.java
+++ src/codeu/chat/client/core/View.java
@@ -46,27 +46,6 @@ final class View implements BasicView {
     this.source = source;
   }
 
-//   public ServerInfo getInfo() {
-//   try (final Connection connection = source.connect()) {
-//     Serializers.INTEGER.write(connection.out(), NetworkCode.SERVER_INFO_REQUEST);
-//     if (Serializers.INTEGER.read(connection.in()) == NetworkCode.SERVER_INFO_RESPONSE) {
-//       final Time startTime = Time.SERIALIZER.read(connection.in());
-//       return new ServerInfo(startTime);
-//     } else {
-//       // Communicate this error - the server did not respond with the type of
-//       // response we expected.
-//       LOG.error("Response from server failed.");
-//     }
-//   } catch (Exception ex) {
-//     // Communicate this error - something went wrong with the connection.
-//       System.out.println("ERROR: Exception during call on server. Check log for details.");
-//       LOG.error(ex, "Exception during call on server. There is something wrong with the connection.");
-//   }
-//   // If we get here it means something went wrong and null should be returned
-//   LOG.error("Something went wrong.");
-//   return null;
-// }
-
   @Override
   public Collection<User> getUsers() {
 
